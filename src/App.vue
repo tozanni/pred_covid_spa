4e94df9e9badda2723e2f6fdc685a79499adcf7e
Cesar Contreras <hi@cerco.dev>
2020-05-19 14:34:33 -0700

    add navigation buttons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
   <v-app>
     <v-navigation-drawer app clipped left v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Covid 19
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Calculadora de Probabilidad RcP
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
       <v-list dense>
         <v-list-item :to="{ name: 'home' }" link>
           <v-list-item-action>
-            <v-icon>mdi-exit-to-app</v-icon>
+            <v-icon>mdi-home-outline</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title>Inicio</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <v-list-item :to="{ name: 'home' }" link>
+          <v-list-item-action>
+            <v-icon>mdi-information-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Proyecto</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :to="{ name: 'home' }" link>
+          <v-list-item-action>
+            <v-icon>mdi-card-account-mail-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contacto</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn color="light-blue darken-4 white--text" block><v-icon left>mdi-exit-to-app</v-icon> cerrar sesion</v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
     <v-app-bar app clipped-right color="primary" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
