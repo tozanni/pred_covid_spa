0fc87a4ad495ad725ea29008c3fa95b701c0c3f5
Cesar Contreras <hi@cerco.dev>
2020-05-19 17:25:42 -0700

    adds about and contact views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>
+      Sobre el proyecto: En el Tec de Monterrey queremos desarrollar una
+      aplicación de “machine learning” que ayude a determinar la probabilidad de
+      deterioro de pacientes enfermos de COVID-19 que apoye a los hospitales a
+      atenderlos en esta crisis sanitaria. Los usuarios son doctores,
+      enfermeras, capturistas y tomadores de decisiones en hospitales atendiendo
+      pacientes enfermos de COVID-19.
+    </p>
+
+    <p>
+      <strong>Objetivo:</strong> Dar visibilidad de unos días de anticipación
+      para prepararse con camas y equipo necesario para atender a pacientes con
+      probable deterioro. Apoyar a los doctores para su toma de decisiones con
+      esta probabilidad de deterioro basada en los casos registrados. Registrar
+      datos que sirvan para hacer el mapeo de características o variables
+      específicas a deterioro.
+    </p>
+
+    <h2>Voluntarios:</h2>
+    <h3>Coordinacion de Proyecto:</h3>
+    Tec de Monterrey
+    <h3>UX Research and UI Design: </h3>
+    Catalina Estrada (Usaria)
+    Natalia Carlos Treviño (Estudiante de UX)
+    <h3>Data science:</h3>
+    Edenred
+    <h3>Front-End</h3>
+    <h3>Back-End</h3>
+    <h3>Traducciones</h3>
   </div>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,15 @@
 <template>
   <v-form>
-    <v-text-field
-      label="Correo"
-      outlined
-      readonly
-    ></v-text-field>
-    <v-text-field
-      label="Contraseña"
-      outlined
-      readonly
-    ></v-text-field>
-    <v-btn :to="{name: 'home'}" color="primary" x-large rounded>
-        iniciar sesion
-      </v-btn>
-      <br />
-      <br />
-      <v-btn :to="{name: 'registration'}" color="primary" x-large text>
-        registrarme
-      </v-btn>
+    <v-text-field label="Correo" outlined readonly></v-text-field>
+    <v-text-field label="Contraseña" outlined readonly></v-text-field>
+    <v-btn :to="{ name: 'home' }" color="primary" x-large rounded>
+      iniciar sesion
+    </v-btn>
+    <br />
+    <br />
+    <v-btn :to="{ name: 'registration' }" color="primary" x-large text>
+      registrarme
+    </v-btn>
   </v-form>
 </template>
 
