66230e6ef33b2d79d752974a3996d7ef263804a4
Darival <darivalrcv@gmail.com>
2020-07-08 01:47:04 -0500

    move triage lists to common file

--- src/views/Triage.vue
+++ src/views/Triage.vue
@@ -49,7 +49,7 @@
       <div class="spacer"></div>
       <div class="d-flex justify-center">
         <v-btn color="primary" x-large rounded @click="submitTriageSigns">
-          <v-icon left large dark>mdi-chevron-right</v-icon>Calcular Probabilidad RCP
+          <v-icon left large dark>mdi-plus-right</v-icon>Calcular Probabilidad RCP
         </v-btn>
       </div>
     </v-form>
@@ -59,6 +59,7 @@
 <script>
 import { mapState, mapActions } from "vuex";
 import { HTTP } from "../http-common";
+import { symptomsList, comorbiditiesList } from "../common/triageLists";
 
 export default {
   computed: {
@@ -95,134 +96,8 @@ export default {
         pregnant: false
       },
       sliderLabels: ["No", "Leve", "Moderado", "Grave"],
-      symptomsList: [
-        {
-          value: "conjunctivitis",
-          text: "Conjuntivitis"
-        },
-        {
-          value: "diarrhea",
-          text: "Diarrea"
-        },
-        {
-          value: "articulations_pain",
-          text: "Dolor de articulaciones"
-        },
-        {
-          value: "muscle_pain",
-          text: "Dolor muscular"
-        },
-        {
-          value: "sore_throat_or_pain",
-          text: "Dolor o ardor de garganta"
-        },
-        {
-          value: "shaking_chills",
-          text: "Escalofríos"
-        },
-        {
-          value: "fatigue_and_weakness",
-          text: "Fatiga y debilidad"
-        },
-        {
-          value: "nausea",
-          text: "Náusea"
-        },
-        {
-          value: "sweating",
-          text: "Sudoración"
-        },
-        {
-          value: "threw_up",
-          text: "Vómito"
-        }
-      ],
-      comorbiditiesList: [
-        {
-          value: "anemia",
-          text: "Anemia"
-        },
-        {
-          value: "coronary_atherosclerosis",
-          text: "Aterosclerosis coronaria"
-        },
-        {
-          value: "cancer",
-          text: "Cáncer"
-        },
-        {
-          value: "cardiovascular",
-          text: "Cardiovascular"
-        },
-        {
-          value: "diabetes_1_and_2",
-          text: "Diabetes 1 y 2"
-        },
-        {
-          value: "gestational_diabetes",
-          text: "Diabetes gestacional"
-        },
-        {
-          value: "cardiac_arrhythmias",
-          text: "Arritmias cardíacas"
-        },
-        {
-          value: "seizures_or_epilepsy",
-          text: "Convulsiones o Epilepsia"
-        },
-        {
-          value: "glaucoma",
-          text: "Glaucoma"
-        },
-        {
-          value: "hepatic_cirrhosis",
-          text: "Cirrosis Hepática"
-        },
-        {
-          value: "hypertension",
-          text: "Hipertensión"
-        },
-        {
-          value: "immunological",
-          text: "Inmunológica"
-        },
-        {
-          value: "leukemia",
-          text: "Leucemia"
-        },
-        {
-          value: "neurological",
-          text: "Neurológica"
-        },
-        {
-          value: "osteoporosis",
-          text: "Osteoporosis"
-        },
-        {
-          value: "parkinson_s",
-          text: "Parkinson"
-        },
-        {
-          value: "high_pressure",
-          text: "Presion Alta"
-        },
-        {
-          value: "pulmonary",
-          text: "Pulmonar"
-        },
-        {
-          value: "chronic_kidney",
-          text: "Renal crónica"
-        },
-        {
-          value: "immunosuppressive_treatment",
-          text: "Tratamiento inmunosupresor"
-        },
-        {
-          value: "hiv",
-          text: "VIH"
-        }
-      ]
+      symptomsList,
+      comorbiditiesList
     };
   },
   methods: {
