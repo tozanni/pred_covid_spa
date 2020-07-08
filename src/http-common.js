036bebdfa05a32f7cfa09c3d93ccb6f0675b09c2
Enrique García <enrique@inodata.com.mx>
2020-07-08 03:24:20 -0500

    Actualiza dominio temporal bluemed.mx

--- src/http-common.js
+++ src/http-common.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const HTTP = axios.create({
     //baseURL: `${process.env.API_REST}`,
-    baseURL: "http://ec2-3-21-99-253.us-east-2.compute.amazonaws.com/api/v1/",
+    baseURL: "https://bluemed.mx/api/v1/",
 });
