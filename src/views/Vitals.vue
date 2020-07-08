7269504eabe204450961a23f5ce735bed8ed9e57
Darival <darivalrcv@gmail.com>
2020-07-08 01:46:38 -0500

    finish vital signs validation rules

--- src/views/Vitals.vue
+++ src/views/Vitals.vue
@@ -1,96 +1,154 @@
 <template>
-  <v-container fluid>
-    <ValidationProvider
-      name="edad"
-      rules="required|numeric|between:1,120"
-      v-slot="{ errors, valid }"
-    >
-      <v-text-field
-        :success="valid"
-        id="edad"
-        label="Edad:"
-        v-model.number="form.age"
-        suffix="años"
-        type="number"
-        :error-messages="errors"
-        :error="errors.length > 0"
-      ></v-text-field>
-    </ValidationProvider>
-    <ValidationProvider rules="required" name="gender" v-slot="{ errors, valid }">
-      <v-radio-group
-        id="gender"
-        v-model="form.gender"
-        label="Sexo:"
-        :success="valid"
-        :error-messages="errors"
+<ValidationObserver slim v-slot="{ invalid }">
+  <form action>
+    <v-container fluid>
+      <ValidationProvider
+        name="edad"
+        rules="required|numeric|between:1,120"
+        v-slot="{ errors, valid }"
       >
-        <v-radio label="Masculino" value="M"></v-radio>
-        <v-radio label="Femenino" value="F"></v-radio>
-      </v-radio-group>
-    </ValidationProvider>
-    <ValidationProvider rules="required|between:1,500" name="weight" v-slot="{ errors, valid }">
-      <v-text-field
-        id="weight"
-        label="Peso:"
-        v-model.number="form.weight"
-        suffix="kg"
-        :success="valid"
-        :error-messages="errors"
-      ></v-text-field>
-    </ValidationProvider>
-    <ValidationProvider rules="required|between:40,250" name="height" v-slot="{ errors, valid }">
-      <v-text-field
-        id="height"
-        label="Estatura:"
-        v-model.number="form.height"
-        suffix="cm"
-        :success="valid"
-        :error-messages="errors"
-      ></v-text-field>
-    </ValidationProvider>
-    <p class="text-center" v-show="form.weight !== null && form.height !== null">
-      <Span class="font-weight-bold">IMC</Span>
-      <!-- <span class="amber--text text--darken-3">Sobrepeso</span> -->
-      <span>{{(form.weight / (form.height / 100 * form.height / 100)).toFixed(2) }}</span>
-    </p>
-    <v-text-field
-      label="T.A. Diastolica:"
-      v-model.number="form.diastolic_blood_pressure"
-      suffix="mmhg"
-    ></v-text-field>
-    <v-text-field
-      label="T.A. Sistolica:"
-      v-model.number="form.systolic_blood_pressure"
-      suffix="mmhg"
-    ></v-text-field>
-    <v-text-field
-      label="Frecuencia Cardiaca:"
-      v-model.number="form.heart_rate"
-      suffix="latidos/min"
-    ></v-text-field>
-    <v-text-field
-      label="Frecuencia Respiratoria:"
-      v-model.number="form.breathing_frequency"
-      suffix="respiraciones/min"
-    ></v-text-field>
-    <v-text-field label="Temperatura:" v-model.number="form.temperature" suffix="°C"></v-text-field>
-    <ValidationProvider rules="required|between:10,99" name="oximetry" v-slot="{ errors, valid }">
-      <v-text-field
-        id="oximetry"
-        label="Oximetria:"
-        v-model.number="form.oximetry"
-        suffix="%"
-        :success="valid"
-        :error-messages="errors"
-      ></v-text-field>
-    </ValidationProvider>
-    <v-text-field label="Glucosa Capilar:" v-model.number="form.capillary_glucose" suffix="mg/dl"></v-text-field>
-    <div class="d-flex justify-center">
-      <v-btn color="primary" x-large rounded @click="submitVitalSigns()">
-        <v-icon left large dark>mdi-chevron-right</v-icon>Triage
-      </v-btn>
-    </div>
-  </v-container>
+        <v-text-field
+          :success="valid"
+          id="edad"
+          label="Edad:"
+          v-model.number="form.age"
+          suffix="años"
+          type="number"
+          :error-messages="errors"
+          :error="errors.length > 0"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider rules="required" name="gender" v-slot="{ errors, valid }">
+        <v-radio-group
+          id="gender"
+          v-model="form.gender"
+          label="Sexo:"
+          :success="valid"
+          :error-messages="errors"
+        >
+          <v-radio label="Masculino" value="M"></v-radio>
+          <v-radio label="Femenino" value="F"></v-radio>
+        </v-radio-group>
+      </ValidationProvider>
+      <ValidationProvider rules="required|between:1,500" name="weight" v-slot="{ errors, valid }">
+        <v-text-field
+          id="weight"
+          label="Peso:"
+          v-model.number="form.weight"
+          suffix="kg"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider rules="required|between:40,250" name="height" v-slot="{ errors, valid }">
+        <v-text-field
+          id="height"
+          label="Estatura:"
+          v-model.number="form.height"
+          suffix="cm"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <p class="text-center" v-show="form.weight !== null && form.height !== null">
+        <Span class="font-weight-bold">IMC</Span>
+        <span>{{(form.weight / (form.height / 100 * form.height / 100)).toFixed(2) }}</span>
+      </p>
+      <ValidationProvider
+        rules="required|between:0,250"
+        name="diastolic_blood_pressure"
+        v-slot="{ errors, valid }"
+      >
+        <v-text-field
+          label="T.A. Diastolica:"
+          v-model.number="form.diastolic_blood_pressure"
+          suffix="mmhg"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider
+        rules="required|between:0,250"
+        name="systolic_blood_pressure"
+        v-slot="{ errors, valid }"
+      >
+        <v-text-field
+          label="T.A. Sistolica:"
+          v-model.number="form.systolic_blood_pressure"
+          suffix="mmhg"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider rules="required|between:0,300" name="heart_rate" v-slot="{ errors, valid }">
+        <v-text-field
+          id="heart_rate"
+          label="Frecuencia Cardiaca:"
+          v-model.number="form.heart_rate"
+          suffix="latidos/min"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider
+        rules="required|between:0,800"
+        name="breathing_frequency"
+        v-slot="{ errors, valid }"
+      >
+        <v-text-field
+          id="breathing_frequency"
+          label="Frecuencia Respiratoria:"
+          v-model.number="form.breathing_frequency"
+          suffix="respiraciones/min"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider
+        rules="required|between:0,800"
+        name="temperature"
+        v-slot="{ errors, valid }"
+      >
+        <v-text-field
+          id="temperature"
+          label="Temperatura:"
+          v-model.number="form.temperature"
+          suffix="°C"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider rules="required|between:10,99" name="oximetry" v-slot="{ errors, valid }">
+        <v-text-field
+          id="oximetry"
+          label="Oximetria:"
+          v-model.number="form.oximetry"
+          suffix="%"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <ValidationProvider
+        rules="required|between:10,99"
+        name="capillary_glucose"
+        v-slot="{ errors, valid }"
+      >
+        <v-text-field
+          label="Glucosa Capilar:"
+          v-model.number="form.capillary_glucose"
+          suffix="mg/dl"
+          :success="valid"
+          :error-messages="errors"
+        ></v-text-field>
+      </ValidationProvider>
+      <div class="d-flex justify-center">
+        <v-btn color="primary" x-large rounded @click="submitVitalSigns()" :disabled="invalid">
+          <v-icon left large dark>mdi-chevron-right</v-icon>Triage
+        </v-btn>
+      </div>
+    </v-container>
+  </form>
+</ValidationObserver>
 </template>
 
 <script>
@@ -111,7 +169,9 @@ export default {
         admission_date: this.record.admission_date,
         status: this.record.status,
         vital_signs: {
-          age: moment().subtract(this.form.age, 'years').format("YYYY-MM-DD HH:mm:ss"),
+          age: moment()
+            .subtract(this.form.age, "years")
+            .format("YYYY-MM-DD HH:mm:ss"),
           gender: this.form.gender,
           weight: this.form.weight,
           height: this.form.height,
@@ -165,7 +225,7 @@ export default {
   },
   mounted() {
     if (this.record && this.record.vital_signs) {
-      this.form = {...this.record.vital_signs};
+      this.form = { ...this.record.vital_signs };
     }
   }
 };
