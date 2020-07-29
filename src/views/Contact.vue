7d35c2c5b725187d6abb453d3573f45ccaa19d01
Darival <darivalrcv@gmail.com>
2020-07-28 20:48:55 -0500

    add contact message

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,35 +1,96 @@
 <template>
-<v-container>
-  <v-form>
-    <v-text-field label="Nombre" solo></v-text-field>
-    <v-select
-          :items="items"
-          label="Asunto"
-          solo
-        ></v-select>
-    <v-text-field label="Correo" solo></v-text-field>
-    <v-textarea solo name="input-7-4" label="¿Qué quieres decir?"></v-textarea>
+  <v-container>
+    <ValidationObserver slim v-slot="{ invalid }">
+      <v-form>
+        <ValidationProvider rules="required" name="name" v-slot="{ errors, valid }">
+          <v-text-field
+            label="Nombre"
+            solo
+            v-model="form.name"
+            :success="valid"
+            :error-messages="errors"
+          ></v-text-field>
+        </ValidationProvider>
+        <ValidationProvider rules="required" name="subject" v-slot="{ errors, valid }">
+          <v-select
+            :items="items"
+            label="Asunto"
+            solo
+            v-model="form.subject"
+            :success="valid"
+            :error-messages="errors"
+          ></v-select>
+        </ValidationProvider>
+        <ValidationProvider rules="required|email" name="email" v-slot="{ errors, valid }">
+          <v-text-field
+            label="Correo"
+            solo
+            :success="valid"
+            v-model="form.email"
+            :error-messages="errors"
+          ></v-text-field>
+        </ValidationProvider>
+        <ValidationProvider rules="required" name="message" v-slot="{ errors, valid }">
+          <v-textarea
+            solo
+            name="input-7-4"
+            label="¿Qué quieres decir?"
+            v-model="form.message"
+            :success="valid"
+            :error-messages="errors"
+          ></v-textarea>
+        </ValidationProvider>
+        <v-btn color="primary" x-large rounded @click="submit()" :disabled="invalid">
+          <v-icon left large dark>mdi-chevron-right</v-icon>Enviar
+        </v-btn>
+      </v-form>
+    </ValidationObserver>
 
-    <v-btn color="primary" x-large rounded>
-      <v-icon left large dark>mdi-chevron-right</v-icon>
-      Enviar
-    </v-btn>
-  </v-form>
-</v-container>
+    <v-snackbar v-model="snackbar" color="#48bb78">
+      Gracias por tu mensaje.
+      <template v-slot:action="{ attrs }">
+        <v-btn text v-bind="attrs" @click="snackbar = false">Cerrar</v-btn>
+      </template>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import "../common/validation-rules";
+import { HTTP } from "../http-common";
+
 export default {
-    data() {
-        return {
-            items: [
-                'Quiero colaborar',
-                'Retroalimentacion',
-                'Reportar Error',
-                'Otro (Escribir)'
-            ]
-        }
-    }
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+  data() {
+    return {
+      form: {
+        name: null,
+        email: null,
+        subject: null,
+        message: null,
+      },
+      snackbar: false,
+      items: [
+        "Quiero colaborar",
+        "Retroalimentacion",
+        "Reportar Error",
+        "Otro (Escribir)",
+      ],
+    };
+  },
+  methods: {
+    submit() {
+      HTTP.post('contact', this.form)
+        .then((res) => {
+          this.snackbar = true;
+        })
+        .catch((error) => console.error(error));
+    },
+  },
 };
 </script>
 
