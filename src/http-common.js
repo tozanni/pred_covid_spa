56affed943e4b261bc9929409153b306b67d8440
Enrique García <enrique@inodata.com.mx>
2020-07-30 02:22:05 -0500

    Establece API_REST default

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const HTTP = axios.create({
-    baseURL: process.env.API_REST,
+    baseURL: process.env.NODE_ENV === 'production' ?
+        "https://api.rcpcovid19.mx/api/v1/" : process.env.API_REST,
 });
