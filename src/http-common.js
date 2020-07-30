faf4aa9821762ee33835d5d57d8a347e12590384
Enrique García <enrique@inodata.com.mx>
2020-07-30 03:33:00 -0500

    api_rest

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
-export const HTTP = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ?
-        "https://api.rcpcovid19.mx/api/v1/" : `${process.env.API_REST}`,
-});
+let api_rest = {};
+switch (process.env.NODE_ENV) {
+    case 'production':
+        api_rest.baseURL = "https://api.rcpcovid19.mx/api/v1/"
+        break;
+    case 'dev':
+        api_rest.baseURL = "https://api-dev.rcpcovid19.mx/api/v1/"
+        break;
+    default:
+        `${process.env.API_REST}`
+}
+
+export const HTTP = axios.create(api_rest);
